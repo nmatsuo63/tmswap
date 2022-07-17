e67d0a91dc529ad1fa21dbfeb5052ed63c42e6a9
naom <nmatsuo.63@gmail.com>
2022-07-17 14:08:15 +0900

    buyToken comment

--- contracts/dex.sol
+++ contracts/dex.sol
@@ -9,9 +9,13 @@ contract Dex {
     // 購入したいトークンコントラクトのアドレス_tokenAddr、トークン購入のためにに支払う必要のあるETH_cost、購入したいトークン量_amount
     // DEXコントラクトにETHを送金するため、payable修飾子をつける
     function  buyToken(address _tokenAddr, uint256 _cost, uint256 _amount) external payable {
+        // ERC20コントラクトを継承したトークンコントラクトのアドレスに、_tokenAddrを型キャストして格納
         ERC20 token = ERC20(_tokenAddr);
+        // ユーザが支払ったETHがトークンを購入するためのcost以上かを確認
         require(msg.value >= _cost, "Insufficient ETH");
+        // ユーザが購入するトークンがDEXコントラクト内に十分存在するかを確認
         require(token.balanceOf(address(this)) >= _amount, "Token sold out");
+        // msg.senderに対し、トークンを_amountだけ転送
         token.transfer(msg.sender, _amount);
     }
 
